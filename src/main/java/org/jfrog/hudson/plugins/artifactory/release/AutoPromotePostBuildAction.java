6c59b55c00d24ae47a7d39a3648afb4a2cff1a73
Mark Pope <scobal@gmail.com>
2013-07-26 15:37:49 +0100

    Working promotion with hardcoded targetStatus and repositoryKey

--- src/main/java/org/jfrog/hudson/plugins/artifactory/release/AutoPromotePostBuildAction.java
+++ src/main/java/org/jfrog/hudson/plugins/artifactory/release/AutoPromotePostBuildAction.java
@@ -1,14 +1,14 @@
 package org.jfrog.hudson.plugins.artifactory.release;
 
+import com.google.common.collect.Lists;
 import hudson.Extension;
 import hudson.Launcher;
-import hudson.model.AbstractBuild;
-import hudson.model.BuildListener;
-import hudson.model.BuildableItemWithBuildWrappers;
-import hudson.model.User;
+import hudson.model.*;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
+import org.jfrog.hudson.plugins.artifactory.ArtifactoryBuilder;
 import org.jfrog.hudson.plugins.artifactory.action.ActionableHelper;
 import org.jfrog.hudson.plugins.artifactory.config.ArtifactoryServer;
 import org.jfrog.hudson.plugins.artifactory.config.Credentials;
@@ -16,10 +16,12 @@ import org.jfrog.hudson.plugins.artifactory.gradle.ArtifactoryGradleConfigurator
 import org.jfrog.hudson.plugins.artifactory.util.CredentialResolver;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.util.List;
+
 /**
  * @author Mark Pope
  */
-public class AutoPromotePostBuildAction extends Publisher {
+public class AutoPromotePostBuildAction extends Notifier {
 
     private String targetStatus;
     private String repositoryKey;
@@ -36,16 +38,54 @@ public class AutoPromotePostBuildAction extends Publisher {
         this.useCopy = useCopy;
     }
 
+    public String getTargetStatus() {
+        return targetStatus;
+    }
+
+    public String getRepositoryKey() {
+        return repositoryKey;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public boolean isIncludeDependencies() {
+        return includeDependencies;
+    }
+
+    public boolean isUseCopy() {
+        return useCopy;
+    }
+
+
     public BuildStepMonitor getRequiredMonitorService() {
-        return null;
+        return BuildStepMonitor.BUILD;
+    }
+
+    public List<String> getTargetStatuses() {
+        return Lists.newArrayList(/*"Staged", */"Released", "Rolled-back");
+    }
+
+    public List<String> getRepositoryKeys() {
+
+        ArtifactoryServer artifactoryServer = null;//getArtifactoryServer();
+        if (artifactoryServer == null) {
+            return Lists.newArrayList();
+        }
+        List<String> repos = artifactoryServer.getReleaseRepositoryKeysFirst();
+        repos.add(0, "");  // option not to move
+        return repos;
     }
 
     @Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        ArtifactoryGradleConfigurator configurator = ActionableHelper.getBuildWrapper((BuildableItemWithBuildWrappers) build.getProject(), ArtifactoryGradleConfigurator.class);
-        ArtifactoryServer artifactoryServer = configurator.getArtifactoryServer();
-        Credentials deployer = CredentialResolver.getPreferredDeployer(artifactoryServer);
+        ArtifactoryServer artifactoryServer = getArtifactoryServer(build.getProject());
+        Credentials deployer = getDeployerCredentials(artifactoryServer);
         String ciUser = getCiUser();
+
+        targetStatus = "Released";
+        repositoryKey = "bi-infrastructure-ng-release-local";
 
         PromotionConfig promotionConfig = new PromotionConfig(targetStatus, repositoryKey, comment, ciUser, useCopy, includeDependencies);
         ArtifactoryPromoter promoter = new ArtifactoryPromoter(build, null, promotionConfig, artifactoryServer, deployer);
@@ -58,13 +98,22 @@ public class AutoPromotePostBuildAction extends Publisher {
         }
     }
 
+    private Credentials getDeployerCredentials(ArtifactoryServer artifactoryServer) {
+        return CredentialResolver.getPreferredDeployer(artifactoryServer);
+    }
+
+    private ArtifactoryServer getArtifactoryServer(AbstractProject project) {
+        ArtifactoryGradleConfigurator configurator = ActionableHelper.getBuildWrapper((BuildableItemWithBuildWrappers) project, ArtifactoryGradleConfigurator.class);
+        return configurator.getArtifactoryServer();
+    }
+
     private String getCiUser() {
         User user = User.current();
         return (user == null) ? "anonymous" : user.getId();
     }
 
     @Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor {
+    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher>  {
 
         @Override
         public String getDisplayName() {
@@ -75,5 +124,10 @@ public class AutoPromotePostBuildAction extends Publisher {
         public boolean isApplicable(Class arg0) {
             return true;
         }
+
+        public List<String> getTargetStatuses() {
+            return Lists.newArrayList(/*"Staged", */"Released", "Rolled-back");
+        }
+
     }
 }
